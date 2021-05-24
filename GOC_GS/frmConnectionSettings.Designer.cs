a11385a2efe66f9992892c1338a8556348682abe
Efraim Basilio <basilio.efraim@gmail.com>
2021-05-24 22:12:55 +0800

    Add Connection Settings UI

---
+++ GOC_GS/frmConnectionSettings.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace GOC_GS
+{
+    partial class frmConnectionSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDSN = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSaveConfiguration = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DimGray;
+            this.label6.Location = new System.Drawing.Point(160, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 29);
+            this.label6.TabIndex = 1052;
+            this.label6.Text = "Settings";
+            // 
+            // txtDSN
+            // 
+            this.txtDSN.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDSN.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDSN.Location = new System.Drawing.Point(165, 62);
+            this.txtDSN.Name = "txtDSN";
+            this.txtDSN.Size = new System.Drawing.Size(200, 23);
+            this.txtDSN.TabIndex = 1038;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 16);
+            this.label4.TabIndex = 1049;
+            this.label4.Text = "Data Source Name : *";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnClose.Location = new System.Drawing.Point(250, 246);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(115, 28);
+            this.btnClose.TabIndex = 1044;
+            this.btnClose.Text = "Clos&e";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(165, 129);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 23);
+            this.txtName.TabIndex = 1040;
+            // 
+            // txtHost
+            // 
+            this.txtHost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHost.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHost.Location = new System.Drawing.Point(165, 95);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(200, 23);
+            this.txtHost.TabIndex = 1039;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 16);
+            this.label3.TabIndex = 1046;
+            this.label3.Text = "Database Name : *";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 1045;
+            this.label2.Text = "Host : *";
+            // 
+            // btnSaveConfiguration
+            // 
+            this.btnSaveConfiguration.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnSaveConfiguration.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveConfiguration.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveConfiguration.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSaveConfiguration.Location = new System.Drawing.Point(39, 246);
+            this.btnSaveConfiguration.Name = "btnSaveConfiguration";
+            this.btnSaveConfiguration.Size = new System.Drawing.Size(115, 28);
+            this.btnSaveConfiguration.TabIndex = 1043;
+            this.btnSaveConfiguration.Text = "Save";
+            this.btnSaveConfiguration.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSaveConfiguration.UseVisualStyleBackColor = false;
+            this.btnSaveConfiguration.Click += new System.EventHandler(this.btnSaveConfiguration_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(165, 200);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(200, 23);
+            this.txtPassword.TabIndex = 1042;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUser.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(165, 162);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(200, 23);
+            this.txtUser.TabIndex = 1041;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 1048;
+            this.label1.Text = "Password :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(68, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 16);
+            this.label5.TabIndex = 1047;
+            this.label5.Text = "Username : *";
+            // 
+            // frmConnectionSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 308);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDSN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSaveConfiguration);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Name = "frmConnectionSettings";
+            this.Text = "frmConnectionSettings";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmConnectionSettings_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDSN;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Button btnSaveConfiguration;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ GOC_GS/frmConnectionSettings.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GOC_GS
+{
+    public partial class frmConnectionSettings : Form
+    {
+        public frmConnectionSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void GetServerSettings()
+        {
+            txtDSN.Text = GOC_GS.Config.DSN;
+            txtHost.Text = GOC_GS.Config.DB_HOST;
+            txtName.Text = GOC_GS.Config.DB_NAME;
+            txtUser.Text = GOC_GS.Config.DB_USER;
+            txtPassword.Text = GOC_GS.Config.DB_PASSWORD;
+        }
+
+        private bool CheckRequiredFields()
+        {
+            if (txtDSN.Text == "")
+            {
+                return false;
+            }
+
+            if (txtHost.Text == "")
+            {
+                return false;
+            }
+
+            if (txtName.Text == "")
+            {
+                return false;
+            }
+
+            if (txtUser.Text == "")
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void setThis()
+        {
+            if (this.CheckRequiredFields() == true)
+            {
+                //set properties
+                GOC_GS.Config.DSN = txtDSN.Text;
+                GOC_GS.Config.DB_HOST = txtHost.Text;
+                GOC_GS.Config.DB_NAME = txtName.Text;
+                GOC_GS.Config.DB_USER = txtUser.Text;
+                GOC_GS.Config.DB_PASSWORD = txtPassword.Text;
+
+                //save settings
+                GOC_GS.Config.saveSettings();
+
+                if (GOC_GS.Config.TestConnection())
+                {
+                    MessageBox.Show("Successfully Connected to Database", "System Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    //if (flag == "Client")
+                    //{
+                    //    frmFrontMain frmFrontMain = new frmFrontMain();
+                    //    frmFrontMain.ShowDialog();
+                    //    this.Dispose();
+                    //    this.Close();
+                    //}
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill out all required fields", "System Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void frmConnectionSettings_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+                this.Dispose();
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                btnSaveConfiguration.PerformClick();
+            }
+        }
+
+        private void btnSaveConfiguration_Click(object sender, EventArgs e)
+        {
+            setThis();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
