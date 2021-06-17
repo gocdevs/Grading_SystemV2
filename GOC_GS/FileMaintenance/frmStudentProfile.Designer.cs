b705abcb2955417b0ced9d0a3c059d5ab2f8a367
Rodel Ramos <ramosrodel31lds@gmail.com>
2021-06-17 14:13:43 +0800

    add form Student Profile

---
+++ GOC_GS/FileMaintenance/frmStudentProfile.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace GOC_GS.FileMaintenance
+{
+    partial class frmStudentProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.PictureBox();
+            this.txtSCode = new System.Windows.Forms.TextBox();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvList
+            // 
+            this.dgvList.AllowUserToAddRows = false;
+            this.dgvList.AllowUserToDeleteRows = false;
+            this.dgvList.BackgroundColor = System.Drawing.Color.White;
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Location = new System.Drawing.Point(14, 125);
+            this.dgvList.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.ReadOnly = true;
+            this.dgvList.RowHeadersVisible = false;
+            this.dgvList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvList.Size = new System.Drawing.Size(1123, 547);
+            this.dgvList.TabIndex = 7936;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Image = global::GOC_GS.Properties.Resources.refresh;
+            this.btnCancel.Location = new System.Drawing.Point(1052, 73);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 44);
+            this.btnCancel.TabIndex = 7960;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAdd.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Image = global::GOC_GS.Properties.Resources.add;
+            this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdd.Location = new System.Drawing.Point(948, 73);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(98, 44);
+            this.btnAdd.TabIndex = 7959;
+            this.btnAdd.Text = "      &Add New";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Image = global::GOC_GS.Properties.Resources.close;
+            this.btnClose.Location = new System.Drawing.Point(1117, 8);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(20, 30);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnClose.TabIndex = 7958;
+            this.btnClose.TabStop = false;
+            // 
+            // txtSCode
+            // 
+            this.txtSCode.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSCode.Location = new System.Drawing.Point(14, 42);
+            this.txtSCode.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.txtSCode.Name = "txtSCode";
+            this.txtSCode.Size = new System.Drawing.Size(409, 25);
+            this.txtSCode.TabIndex = 7963;
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.lblFile.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFile.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblFile.Location = new System.Drawing.Point(13, 18);
+            this.lblFile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(63, 20);
+            this.lblFile.TabIndex = 7964;
+            this.lblFile.Text = "*File Path";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Image = global::GOC_GS.Properties.Resources.refresh;
+            this.btnExport.Location = new System.Drawing.Point(117, 73);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(85, 44);
+            this.btnExport.TabIndex = 7966;
+            this.btnExport.Text = "&Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // btnImport
+            // 
+            this.btnImport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnImport.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImport.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnImport.Location = new System.Drawing.Point(15, 75);
+            this.btnImport.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(98, 44);
+            this.btnImport.TabIndex = 7965;
+            this.btnImport.Text = "&Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Image = global::GOC_GS.Properties.Resources.refresh;
+            this.btnBrowse.Location = new System.Drawing.Point(436, 42);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(79, 25);
+            this.btnBrowse.TabIndex = 7967;
+            this.btnBrowse.Text = "&Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            // 
+            // frmStudentProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1150, 678);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.txtSCode);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgvList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmStudentProfile";
+            this.Text = "frmStudentProfile";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvList;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.PictureBox btnClose;
+        private System.Windows.Forms.TextBox txtSCode;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
