03a1f901c73da22d96a021eb2023585b047a718c
efraim_basilio <basilio.efraim@gmail.com>
2021-06-21 15:52:03 +0800

    update Textcase

--- GOC_GS/FileMaintenance/frmStudentProfile.cs
+++ GOC_GS/FileMaintenance/frmStudentProfile.cs
@@ -38,19 +38,33 @@ namespace GOC_GS.FileMaintenance
 
         private void btnSaveToDatabase_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < dgvStudentList.Rows.Count; i++)
+            Util_RequiredFields util = new Util_RequiredFields();
+            
+            if (dgvStudentList.RowCount > 0)
             {
-                student_Profile.GOC_No = dgvStudentList.Rows[i].Cells[0].Value.ToString();                         
-                student_Profile.FName = dgvStudentList.Rows[i].Cells[1].Value.ToString();
-                student_Profile.MName = dgvStudentList.Rows[i].Cells[2].Value.ToString();
-                student_Profile.LName = dgvStudentList.Rows[i].Cells[3].Value.ToString();
-                student_Profile.Grade_Level = dgvStudentList.Rows[i].Cells[4].Value.ToString();
-                student_Profile.Section = dgvStudentList.Rows[i].Cells[5].Value.ToString();
-                student_Profile.Strand = dgvStudentList.Rows[i].Cells[7].Value.ToString();
-                student_Profile.Academic_Status = dgvStudentList.Rows[i].Cells[8].Value.ToString();
+                for (int i = 0; i < dgvStudentList.Rows.Count; i++)
+                {
+                    student_Profile.GOC_No = dgvStudentList.Rows[i].Cells[0].Value.ToString();
+                    student_Profile.LRN_No = dgvStudentList.Rows[i].Cells[1].Value.ToString();
+                    student_Profile.FName = util.ToProperCase(dgvStudentList.Rows[i].Cells[2].Value.ToString());
+                    student_Profile.MName = util.ToProperCase(dgvStudentList.Rows[i].Cells[3].Value.ToString());
+                    student_Profile.LName = util.ToProperCase(dgvStudentList.Rows[i].Cells[4].Value.ToString());
+                    student_Profile.Grade_Level = dgvStudentList.Rows[i].Cells[5].Value.ToString();
+                    student_Profile.Section = util.ToProperCase(dgvStudentList.Rows[i].Cells[6].Value.ToString());
+                    student_Profile.Strand = util.ToProperCase(dgvStudentList.Rows[i].Cells[7].Value.ToString());
+                    student_Profile.Academic_Status = util.ToProperCase(dgvStudentList.Rows[i].Cells[8].Value.ToString());
 
-                student_Profile.Save();
+                    student_Profile.Save();
+                }
+
+                MessageBox.Show("Data Successfully Save", "Grading System", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            else
+            {
+                return;
+            }
+
+            
 
            
         }
