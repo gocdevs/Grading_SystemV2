8de10aac787da57fae9db8f4a606bd1dd6ee25fe
Rodel Ramos <ramosrodel31lds@gmail.com>
2021-06-25 17:47:31 +0800

    add form student data

---
+++ GOC_GS/FileMaintenance/frmStudentData.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace GOC_GS.FileMaintenance
+{
+    partial class frmStudentData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbStrand = new System.Windows.Forms.ComboBox();
+            this.cmbGradeLevel = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.PictureBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvStudentList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(542, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7981;
+            this.label4.Text = "Strand";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(415, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 7980;
+            this.label3.Text = "Grade Level";
+            // 
+            // cmbStrand
+            // 
+            this.cmbStrand.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStrand.FormattingEnabled = true;
+            this.cmbStrand.Location = new System.Drawing.Point(545, 50);
+            this.cmbStrand.Name = "cmbStrand";
+            this.cmbStrand.Size = new System.Drawing.Size(121, 21);
+            this.cmbStrand.TabIndex = 7979;
+            this.cmbStrand.SelectedIndexChanged += new System.EventHandler(this.cmbStrand_SelectedIndexChanged);
+            // 
+            // cmbGradeLevel
+            // 
+            this.cmbGradeLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGradeLevel.FormattingEnabled = true;
+            this.cmbGradeLevel.Items.AddRange(new object[] {
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.cmbGradeLevel.Location = new System.Drawing.Point(418, 50);
+            this.cmbGradeLevel.Name = "cmbGradeLevel";
+            this.cmbGradeLevel.Size = new System.Drawing.Size(121, 21);
+            this.cmbGradeLevel.TabIndex = 7978;
+            this.cmbGradeLevel.SelectedIndexChanged += new System.EventHandler(this.cmbGradeLevel_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7977;
+            this.label2.Text = "Filter By";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 7976;
+            this.label1.Text = "Search By Last Name";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtSearch.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.txtSearch.Location = new System.Drawing.Point(15, 50);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(313, 20);
+            this.txtSearch.TabIndex = 7975;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAdd.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdd.Location = new System.Drawing.Point(933, 40);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(98, 44);
+            this.btnAdd.TabIndex = 7972;
+            this.btnAdd.Text = "&Update";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Image = global::GOC_GS.Properties.Resources.close;
+            this.btnClose.Location = new System.Drawing.Point(1110, 4);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(20, 30);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnClose.TabIndex = 7974;
+            this.btnClose.TabStop = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Image = global::GOC_GS.Properties.Resources.refresh;
+            this.btnCancel.Location = new System.Drawing.Point(1037, 40);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 44);
+            this.btnCancel.TabIndex = 7973;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // dgvStudentList
+            // 
+            this.dgvStudentList.AllowUserToAddRows = false;
+            this.dgvStudentList.AllowUserToDeleteRows = false;
+            this.dgvStudentList.BackgroundColor = System.Drawing.Color.White;
+            this.dgvStudentList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudentList.Location = new System.Drawing.Point(7, 92);
+            this.dgvStudentList.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvStudentList.Name = "dgvStudentList";
+            this.dgvStudentList.ReadOnly = true;
+            this.dgvStudentList.RowHeadersVisible = false;
+            this.dgvStudentList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvStudentList.Size = new System.Drawing.Size(1123, 532);
+            this.dgvStudentList.TabIndex = 7982;
+            // 
+            // frmStudentData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 639);
+            this.Controls.Add(this.dgvStudentList);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbStrand);
+            this.Controls.Add(this.cmbGradeLevel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmStudentData";
+            this.Text = "frmStudentData";
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbStrand;
+        private System.Windows.Forms.ComboBox cmbGradeLevel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.PictureBox btnClose;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgvStudentList;
+    }
+}
---
+++ GOC_GS/FileMaintenance/frmStudentData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GOC_GS.Models;
+
+namespace GOC_GS.FileMaintenance
+{
+    public partial class frmStudentData : Form
+    {
+        public frmStudentData()
+        {
+            InitializeComponent();
+            strand.LoadCombo(cmbStrand);
+            sda.initDatagrid(dgvStudentList);
+        }
+
+        StudentData sda = new StudentData();
+        Strand strand = new Strand();
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            sda.Lname = txtSearch.Text;
+            sda.Grade_level = cmbGradeLevel.Text;
+            sda.Strand = cmbStrand.Text;
+            sda.LoadDataTable(dgvStudentList);
+        }
+
+        private void cmbGradeLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sda.Lname = txtSearch.Text;
+            sda.Grade_level = cmbGradeLevel.Text;
+            sda.Strand = cmbStrand.Text;
+            sda.LoadDataTable(dgvStudentList);
+        }
+
+        private void cmbStrand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sda.Lname = txtSearch.Text;
+            sda.Grade_level = cmbGradeLevel.Text;
+            sda.Strand = cmbStrand.Text;
+            sda.LoadDataTable(dgvStudentList);
+        }
+    }
+}
