7ac2955300b1ce64c6ddfc1ed67c6969f288cdf9
efraim_basilio <basilio.efraim@gmail.com>
2021-08-20 16:38:31 +0800

    Update

--- GOC_GS/frmReport.cs
+++ GOC_GS/frmReport.cs
@@ -34,7 +34,7 @@ namespace GOC_GS
             //Report Connection
             Credential();
             ReportDocument reportData = new ReportDocument();
-            reportData.Load(Application.StartupPath + "Reports/StudentList.rpt");
+            reportData.Load(Application.StartupPath + "/Reports/StudentList.rpt");
             //reportData.Load("C:/Users/SERVER/Documents/GitHub/Grading_SystemV2/GOC_GS/StudentList.rpt");
         
 
@@ -60,7 +60,7 @@ namespace GOC_GS
             Credential();
             ReportDocument reportData = new ReportDocument();
             //reportData.Load(Application.StartupPath + "/Reports/rptAssessment.rpt");
-            reportData.Load(Application.StartupPath + "Reports/FinalSemetralGrades.rpt");
+            reportData.Load(Application.StartupPath + "/Reports/FinalSemetralGrades.rpt");
 
 
             CrystalReport_Connection.Instance.SetConnection(ref reportData);
